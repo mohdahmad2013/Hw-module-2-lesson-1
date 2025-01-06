8ca83b37f94c3ab1854a9fd7616dd2163aa1830d
mohdahmad2013 <ziena.saif@gmail.com>
2025-01-06 04:02:34 -0800

    Add files via upload

---
+++ Hw.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age"))
+if age>20:
+    if age>20 and age<30:
+        print("You can enter the club")
+    else:
+        print("You can't enter the club")
+else:
+    print("You are too young for the club")
